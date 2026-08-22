5fe0fa6249d0cadbd49f0cc97661f48167d13eda
GravityGDev <fuzionbukkit+1@gmail.com>
2026-08-22 05:24:01 +0100

    Migrate coinflip balances to MongoDB

--- src/commands/commandList/gamble/coinflip.js
+++ src/commands/commandList/gamble/coinflip.js
@@ -7,6 +7,7 @@
 
 const CommandInterface = require('../../CommandInterface.js');
 const altercoinflip = require('../patreon/altercoinflip.js');
+const mongoNumeric = require('../../../utils/mongoNumeric.js');
 
 const maxBet = 250000;
 const cowoncy = '<:cowoncy:416043450337853441>';
@@ -40,7 +41,6 @@ module.exports = new CommandInterface({
 			msg = p.msg,
 			args = p.args;
 
-		//Syntax Check
 		let bet = 1,
 			arg1 = args[0];
 		if (global.isInt(arg1)) {
@@ -59,15 +59,13 @@ module.exports = new CommandInterface({
 			return;
 		}
 
-		//Get user choice
 		let choice = 'h';
 		if (arg1 != undefined) arg1 = arg1.toLowerCase();
 		if (arg1 == 'heads' || arg1 == 'h' || arg1 == 'head') choice = 'h';
 		else if (arg1 == 'tails' || arg1 == 't' || arg1 == 'tail') choice = 't';
 
-		//Final syntax check
 		if (bet == 0) {
-			p.errorMsg(", You can't bet 0 dum dum!", 3000);
+			p.errorMsg(" , You can't bet 0 dum dum!".trimStart(), 3000);
 			p.setCooldown(5);
 			return;
 		} else if (bet < 0) {
@@ -80,72 +78,87 @@ module.exports = new CommandInterface({
 			return;
 		}
 
-		let sql = 'SELECT money FROM cowoncy WHERE id = ' + msg.author.id + ';';
-		let result = await p.query(sql);
-		if (result[0] == undefined || result[0].money == 0 || (bet != 'all' && result[0].money < bet)) {
+		const balances = await p.mongo.collection('cowoncy');
+		const balance = await balances.findOne(
+			{ id: String(msg.author.id) },
+			{ projection: { money: 1 } }
+		);
+		const money = mongoNumeric.toBigInt(balance?.money || 0);
+		if (money <= 0n) {
 			p.send('**🚫 | ' + p.getName() + "**, You don't have enough cowoncy!", 3000);
 			return;
-		} else {
-			if (bet == 'all') bet = result[0].money;
-
-			if (maxBet && bet > maxBet) {
-				bet = maxBet;
-			} else if (bet <= 0) {
-				p.errorMsg(", you don't have any cowoncy silly!", 3000);
-				p.setCooldown(5);
-				return;
-			}
-
-			let rand = await random(0, 1);
-			let win = false;
-			//tails
-			if (rand == 0 && choice == 't') win = true;
-			//heads
-			else if (rand == 1 && choice == 'h') win = true;
-
-			sql =
-				'UPDATE cowoncy SET money = money ' +
-				(win ? '+' : '-') +
-				' ' +
-				bet +
-				' WHERE id = ' +
-				msg.author.id +
-				';';
-			result = await p.query(sql);
-			if (win) {
-				p.logger.incr('cowoncy', bet, { type: 'coinflip' }, p.msg);
-				p.logger.incr('gamble', 1, { type: 'coinflip' }, p.msg);
-			} else {
-				p.logger.decr('cowoncy', bet * -1, { type: 'coinflip' }, p.msg);
-				p.logger.decr('gamble', -1, { type: 'coinflip' }, p.msg);
-			}
-			let text =
-				'**' +
-				p.getName() +
-				'** spent **' +
-				cowoncy +
-				' ' +
-				p.global.toFancyNum(bet) +
-				'** and chose ' +
-				(choice == 'h' ? '**heads**' : '**tails**');
-			let text2 = text;
-			text2 +=
-				'\nThe coin spins... ' +
-				(win ? (choice == 'h' ? heads : tails) : choice == 'h' ? tails : heads) +
-				' and you ';
-			if (win) text2 += 'won **' + cowoncy + ' ' + p.global.toFancyNum(bet * 2) + '**!!';
-			else text2 += 'lost it all... :c';
-			text += '\nThe coin spins... ' + spin;
-
-			/* Legendary patreon stuff */
-			text = altercoinflip.alter(p.msg.author.id, text);
-			text2 = altercoinflip.alter(p.msg.author.id, text2);
-
-			let message = await p.send(text);
-			setTimeout(function () {
-				message.edit(text2);
-			}, 2000);
-			p.quest('gamble');
 		}
+
+		if (bet == 'all') {
+			bet = Number(money > BigInt(maxBet) ? BigInt(maxBet) : money);
+		} else if (maxBet && bet > maxBet) {
+			bet = maxBet;
+		}
+		if (bet <= 0) {
+			p.errorMsg(", you don't have any cowoncy silly!", 3000);
+			p.setCooldown(5);
+			return;
+		}
+		if (money < BigInt(bet)) {
+			p.send('**🚫 | ' + p.getName() + "**, You don't have enough cowoncy!", 3000);
+			return;
+		}
+
+		const rand = await random(0, 1);
+		const win = (rand == 0 && choice == 't') || (rand == 1 && choice == 'h');
+		let mutation;
+		if (win) {
+			mutation = await mongoNumeric.add(
+				balances,
+				{ id: String(msg.author.id) },
+				'money',
+				bet
+			);
+		} else {
+			mutation = await mongoNumeric.subtractIfEnough(
+				balances,
+				{ id: String(msg.author.id) },
+				'money',
+				bet
+			);
+		}
+		if (!mutation.modifiedCount) {
+			p.send('**🚫 | ' + p.getName() + "**, You don't have enough cowoncy!", 3000);
+			return;
+		}
+
+		if (win) {
+			p.logger.incr('cowoncy', bet, { type: 'coinflip' }, p.msg);
+			p.logger.incr('gamble', 1, { type: 'coinflip' }, p.msg);
+		} else {
+			p.logger.decr('cowoncy', bet * -1, { type: 'coinflip' }, p.msg);
+			p.logger.decr('gamble', -1, { type: 'coinflip' }, p.msg);
+		}
+		let text =
+			'**' +
+			p.getName() +
+			'** spent **' +
+			cowoncy +
+			' ' +
+			p.global.toFancyNum(bet) +
+			'** and chose ' +
+			(choice == 'h' ? '**heads**' : '**tails**');
+		let text2 = text;
+		text2 +=
+			'\nThe coin spins... ' +
+			(win ? (choice == 'h' ? heads : tails) : choice == 'h' ? tails : heads) +
+			' and you ';
+		if (win) text2 += 'won **' + cowoncy + ' ' + p.global.toFancyNum(bet * 2) + '**!!';
+		else text2 += 'lost it all... :c';
+		text += '\nThe coin spins... ' + spin;
+
+		text = altercoinflip.alter(p.msg.author.id, text);
+		text2 = altercoinflip.alter(p.msg.author.id, text2);
+
+		let message = await p.send(text);
+		setTimeout(function () {
+			message.edit(text2);
+		}, 2000);
+		p.quest('gamble');
 	},
 });
