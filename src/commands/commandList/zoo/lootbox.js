552276ed10fd6fe5188359ae44409fb210dad533
GravityGDev <fuzionbukkit+1@gmail.com>
2026-08-22 05:54:09 +0100

    Migrate lootbox opening to MongoDB

--- src/commands/commandList/zoo/lootbox.js
+++ src/commands/commandList/zoo/lootbox.js
@@ -8,7 +8,6 @@
 const CommandInterface = require('../../CommandInterface.js');
 
 const blank = '<:blank:427371936482328596>';
-const _box = '<:box:427352600476647425>';
 const boxShake = '<a:boxshake:427004983460888588>';
 const boxOpen = '<a:boxopen:427019823747301377>';
 const fboxShake = '<a:fboxshake:725570543834759230>';
@@ -33,18 +32,18 @@ module.exports = new CommandInterface({
 
 	appCommands: [
 		{
-			'name': 'lootbox',
-			'type': 1,
-			'description': 'Open a lootbox',
-			'options': [
+			name: 'lootbox',
+			type: 1,
+			description: 'Open a lootbox',
+			options: [
 				{
-					'type': 3,
-					'name': 'count',
-					'description': 'Number of lootboxes: [number, "all", "fabled"]',
+					type: 3,
+					name: 'count',
+					description: 'Number of lootboxes: [number, "all", "fabled"]',
 				},
 			],
-			'integration_types': [0, 1],
-			'contexts': [0, 1, 2],
+			integration_types: [0, 1],
+			contexts: [0, 1, 2],
 		},
 	],
 
@@ -61,69 +60,42 @@ module.exports = new CommandInterface({
 			(this.args.length > 0 && this.args[0].toLowerCase() == 'all') ||
 			(this.options.count && this.options.count.toLowerCase() == 'all')
 		) {
-			let sql = `SELECT boxcount FROM lootbox WHERE id = ${this.msg.author.id};`;
-			let result = await this.query(sql);
-			if (!result[0] || result[0].boxcount <= 0) {
+			const collection = await this.mongo.collection('lootbox');
+			const row = await collection.findOne({ id: String(this.msg.author.id) });
+			if (!row || Number(row.boxcount || 0) <= 0) {
 				this.errorMsg(", you don't have any more lootboxes!");
 				return;
 			}
-			let boxcount = result[0].boxcount;
-			if (boxcount > maxBoxes) boxcount = maxBoxes;
-			await openMultiple(this, boxcount);
+			await openMultiple(this, Math.min(Number(row.boxcount), maxBoxes));
 		} else if (
 			(this.args.length && ['f', 'fabled'].includes(this.args[0].toLowerCase())) ||
 			(this.options.count && ['f', 'fabled'].includes(this.options.count.toLowerCase()))
 		) {
 			await openFabledBox(this);
-		} else await openBox(this);
+		} else {
+			await openBox(this);
+		}
 	},
 });
 
 async function openBox(p) {
-	let sql = `UPDATE lootbox SET boxcount = boxcount - 1 WHERE id = ${p.msg.author.id} AND boxcount > 0;`;
-	sql += `SELECT uid FROM user WHERE id = ${p.msg.author.id};`;
-	let result = await p.query(sql);
-
-	let uid;
-	if (result[0].changedRows == 0) {
+	const uid = await p.global.getUid(p.msg.author.id);
+	const generated = lootboxUtil.getRandomGems(uid, 1);
+	const opened = await consumeAndGrant(p, uid, 'boxcount', 1, generated.gems);
+	if (!opened) {
 		p.errorMsg(", You don't have any lootboxes!", 3000);
 		return;
-	} else if (!result[1][0] || !result[1][0].uid) {
-		sql = `INSERT IGNORE INTO user (id) VALUES (${p.msg.author.id});`;
-		result = await p.query(sql);
-		uid = result.insertId;
-	} else {
-		uid = result[1][0].uid;
 	}
 
-	if (!uid) {
-		p.errorMsg(', It looks like something went wrong...', 3000);
-		return;
-	}
-
-	let gem = lootboxUtil.getRandomGems(uid, 1);
-	let firstGem = gem.gems[Object.keys(gem.gems)[0]].gem;
-	let gemName = firstGem.rank + ' ' + firstGem.type + ' Gem';
-	let text1 =
-		blank + ' **| ' + p.getName() + '** opens a lootbox\n' + boxShake + ' **|** and finds a ...';
-	let text2 =
-		firstGem.emoji +
-		' **| ' +
-		p.getName() +
-		'** opens a lootbox\n' +
-		boxOpen +
-		' **|** and finds a' +
+	const firstGem = generated.gems[Object.keys(generated.gems)[0]].gem;
+	const gemName = `${firstGem.rank} ${firstGem.type} Gem`;
+	const text1 = `${blank} **| ${p.getName()}** opens a lootbox\n${boxShake} **|** and finds a ...`;
+	const text2 =
+		`${firstGem.emoji} **| ${p.getName()}** opens a lootbox\n${boxOpen} **|** and finds a` +
 		(gemName.charAt(0) == 'E' || gemName.charAt(0) == 'U' ? 'n' : '') +
-		' **' +
-		gemName +
-		'**!';
-
-	result = await p.query(gem.sql);
-
-	let msg = await p.send(text1);
-	setTimeout(function () {
-		msg.edit(text2);
-	}, 3000);
+		` **${gemName}**!`;
+	const msg = await p.send(text1);
+	setTimeout(() => msg.edit(text2), 3000);
 }
 
 async function openMultiple(p, count) {
@@ -133,109 +105,82 @@ async function openMultiple(p, count) {
 		return;
 	}
 
-	let sql = `UPDATE IGNORE lootbox SET boxcount = boxcount - ${count} WHERE id = ${p.msg.author.id} AND boxcount >= ${count};`;
-	sql += `SELECT uid FROM user WHERE id = ${p.msg.author.id};`;
-	let result = await p.query(sql);
-
-	let uid;
-	if (result[0].changedRows == 0) {
-		p.errorMsg(", You don't have any lootboxes!", 3000);
-		return;
-	} else if (!result[1][0] || !result[1][0].uid) {
-		sql = `INSERT IGNORE INTO user (id) VALUES (${p.msg.author.id});`;
-		result = await p.query(sql);
-		uid = result.insertId;
-	} else {
-		uid = result[1][0].uid;
-	}
-
-	if (!uid) {
-		p.errorMsg(', It looks like something went wrong...', 3000);
+	const uid = await p.global.getUid(p.msg.author.id);
+	const generated = lootboxUtil.getRandomGems(uid, count);
+	const opened = await consumeAndGrant(p, uid, 'boxcount', count, generated.gems);
+	if (!opened) {
+		p.errorMsg(", You don't have enough lootboxes!", 3000);
 		return;
 	}
 
-	let gems = lootboxUtil.getRandomGems(uid, count);
 	let gemText = '';
-	for (let key in gems.gems) {
-		let gem = gems.gems[key];
+	for (const key in generated.gems) {
+		const gem = generated.gems[key];
 		gemText += gem.gem.emoji + p.global.toSmallNum(gem.count) + ' ';
 	}
-	let text1 =
-		blank +
-		' **| ' +
-		p.getName() +
-		'** opens ' +
-		count +
-		' lootboxes\n' +
-		boxShake +
-		' **|** and finds...';
-	let text2 =
-		blank +
-		' **| ' +
-		p.getName() +
-		'** opens ' +
-		count +
-		' lootboxes\n' +
-		boxOpen +
-		' **|** and finds: ' +
-		gemText;
-
-	result = await p.query(gems.sql);
-
-	let msg = await p.send(text1);
-	setTimeout(function () {
-		msg.edit(text2);
-	}, 3000);
+	const text1 =
+		`${blank} **| ${p.getName()}** opens ${count} lootboxes\n` +
+		`${boxShake} **|** and finds...`;
+	const text2 =
+		`${blank} **| ${p.getName()}** opens ${count} lootboxes\n` +
+		`${boxOpen} **|** and finds: ${gemText}`;
+	const msg = await p.send(text1);
+	setTimeout(() => msg.edit(text2), 3000);
 }
 
 async function openFabledBox(p) {
-	let sql = `UPDATE lootbox SET fbox = fbox - 1 WHERE id = ${p.msg.author.id} AND fbox > 0;`;
-	sql += `SELECT uid FROM user WHERE id = ${p.msg.author.id};`;
-	let result = await p.query(sql);
-
-	let uid;
-	if (result[0].changedRows == 0) {
-		p.errorMsg(", You don't have any lootboxes!", 3000);
-		return;
-	} else if (!result[1][0] || !result[1][0].uid) {
-		sql = `INSERT IGNORE INTO user (id) VALUES (${p.msg.author.id});`;
-		result = await p.query(sql);
-		uid = result.insertId;
-	} else {
-		uid = result[1][0].uid;
-	}
-
-	if (!uid) {
-		p.errorMsg(', It looks like something went wrong...', 3000);
+	const uid = await p.global.getUid(p.msg.author.id);
+	const generated = lootboxUtil.getRandomFabledGems(uid, 1);
+	const opened = await consumeAndGrant(p, uid, 'fbox', 1, generated.gems);
+	if (!opened) {
+		p.errorMsg(", You don't have any Fabled lootboxes!", 3000);
 		return;
 	}
 
-	let gem = lootboxUtil.getRandomFabledGems(uid, 1);
-	let firstGem = gem.gems[Object.keys(gem.gems)[0]].gem;
-	let gemName = firstGem.rank + ' ' + firstGem.type + ' Gem';
-	let text1 =
-		blank +
-		' **| ' +
-		p.getName() +
-		'** opens a Fabled lootbox\n' +
-		fboxShake +
-		' **|** and finds a ...';
-	let text2 =
-		firstGem.emoji +
-		' **| ' +
-		p.getName() +
-		'** opens a Fabled lootbox\n' +
-		fboxOpen +
-		' **|** and finds a' +
+	const firstGem = generated.gems[Object.keys(generated.gems)[0]].gem;
+	const gemName = `${firstGem.rank} ${firstGem.type} Gem`;
+	const text1 = `${blank} **| ${p.getName()}** opens a Fabled lootbox\n${fboxShake} **|** and finds a ...`;
+	const text2 =
+		`${firstGem.emoji} **| ${p.getName()}** opens a Fabled lootbox\n${fboxOpen} **|** and finds a` +
 		(gemName.charAt(0) == 'E' || gemName.charAt(0) == 'U' ? 'n' : '') +
-		' **' +
-		gemName +
-		'**!';
+		` **${gemName}**!`;
+	const msg = await p.send(text1);
+	setTimeout(() => msg.edit(text2), 3000);
+}
 
-	result = await p.query(gem.sql);
+async function consumeAndGrant(p, uid, field, count, gemResult) {
+	const boxes = await p.mongo.collection('lootbox');
+	const userGems = await p.mongo.collection('user_gem');
+	const session = await p.mongo.startSession();
+	let success = false;
+	try {
+		await session.withTransaction(async () => {
+			success = false;
+			const removed = await boxes.updateOne(
+				{ id: String(p.msg.author.id), [field]: { $gte: count } },
+				{ $inc: { [field]: -count } },
+				{ session }
+			);
+			if (!removed.modifiedCount) return;
 
-	let msg = await p.send(text1);
-	setTimeout(function () {
-		msg.edit(text2);
-	}, 3000);
+			for (const key in gemResult) {
+				const row = gemResult[key];
+				await userGems.updateOne(
+					{ uid, gname: row.gem.key },
+					{
+						$inc: { gcount: row.count },
+						$setOnInsert: { uid, gname: row.gem.key, activecount: 0 },
+					},
+					{ upsert: true, session }
+				);
+			}
+			success = true;
+		});
+	} catch (err) {
+		console.error(err);
+		return false;
+	} finally {
+		await session.endSession();
+	}
+	return success;
 }
